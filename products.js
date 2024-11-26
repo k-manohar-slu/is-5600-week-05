fad1e5cc151c2b56b816b425849f578fc8bad8cd
k-manohar-slu <manohar.kancharla@slu.edu>
2024-11-26 21:10:56 +0000

    lab 05 submission

--- products.js
+++ products.js
@@ -1,49 +1,100 @@
-const fs = require('fs').promises
-const path = require('path')
+// orders.js
+const cuid = require('cuid')
 
-const productsFile = path.join(__dirname, 'data/full-products.json')
+const db = require('./db')
+
+const Order = db.model('Order', {
+  _id: { type: String, default: cuid },
+  buyerEmail: { type: String, required: true },
+  products: [{
+    type: String,
+    ref: 'Product', // ref will automatically fetch associated products for us
+    index: true,
+    required: true
+  }],
+  status: {
+    type: String,
+    index: true,
+    default: 'CREATED',
+    enum: ['CREATED', 'PENDING', 'COMPLETED']
+  }
+})
 
 /**
- * List products
- * @param {*} options 
- * @returns 
+ * List orders
+ * @param {Object} options
+ * @returns {Promise<Array>}
  */
 async function list(options = {}) {
 
-  const { offset = 0, limit = 25, tag } = options;
+  const { offset = 0, limit = 25, productId, status } = options;
 
-  const data = await fs.readFile(productsFile)
-  return JSON.parse(data)
-    .filter(product => {
-      if (!tag) {
-        return product
-      }
+  const productQuery = productId ? {
+    products: productId
+  } : {}
 
-      return product.tags.find(({ title }) => title == tag)
-    })
-    .slice(offset, offset + limit) // Slice the products
+  const statusQuery = status ? {
+    status: status
+  } : {}
+
+  const query = {
+    ...productQuery,
+    ...statusQuery
+  }
+
+  const orders = await Order.find(query)
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(limit)
+
+  return orders
 }
 
 /**
- * Get a single product
- * @param {string} id
- * @returns {Promise<object>}
+ * Get an order
+ * @param {Object} order
+ * @returns {Promise<Object>}
  */
-async function get(id) {
-  const products = JSON.parse(await fs.readFile(productsFile))
-
-  // Loop through the products and return the product with the matching id
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id === id) {
-      return products[i]
-    }
-  }
-
-  // If no product is found, return null
-  return null;
+async function get (_id) {
+  // using populate will automatically fetch the associated products.
+  // if you don't use populate, you will only get the product ids
+  const order = await Order.findById(_id)
+    .populate('products')
+    .exec()
+  
+  return order
 }
 
-module.exports = {
-  list,
-  get
+/**
+ * Create an order
+ * @param {Object} order
+ * @returns {Promise<Object>}
+ */
+async function create (fields) {
+  const order = await new Order(fields).save()
+  await order.populate('products')
+  return order
+}
+
+async function edit (_id, change) {
+  const order = await get(_id)
+
+  Object.keys(change).forEach(function (key) {
+    order[key] = change[key]
+  })
+  
+  await order.save()
+  
+  return order
+}
+
+async function destroy (_id) {
+  return await Order.deleteOne({_id})
+}
+
+module.exports ={
+  create,
+  get,
+  edit,
+  destroy,
 }
